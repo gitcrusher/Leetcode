1d554c0f0c9a477cdb400bcb929a76d7680185cb
Aayush Soni <137198498+gitcrusher@users.noreply.github.com>
2026-02-09 12:38:25 +0530

    Time: 35 ms (43.87%), Space: 92.1 MB (58.81%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,19 +1,23 @@
 class StockSpanner {
 public:
     StockSpanner() {}
-    stack<pair<int , int>>st;
+    stack<pair<int , int>>st; // ye isis liye kiya taki sabka span value aur main value ko sath me insert kr sake like for 100 its 1  and more over it would continue
 
     int next(int price) {
-        int span = 1;
-        while(!st.empty()){
-            if(st.top().first<=price){
+        int span = 1; // ye value humne default le li hai 
+        // intusion
+        /*
+            yahan pr hum element ko store krte rshenge aur chek kareneg ki kahan current element ke liye stack ke top me humare pass small value hai aur span jo ki 1 hau usme +1 kr denge kyunki hr elemenet ka default 1 to rahega hi.Moreover hums span ko jo bara value tha jiske karan pop huausme store karenge aur return the span karenge         
+        */
+        while(!st.empty()){ // jabh tak stack empty na ho
+            if(st.top().first<=price){ 
                 span+=st.top().second;
                 st.pop();
             }else{
-                break;
+                break; // for 80,1 after 100,1 the loop if condition wont work aur sedha ye element enter kr jayega kyunki  100 80 se bara hai.
             }
         }
-        st.push({price,span});
+        st.push({price,span});// pehle ye condition likhna parega kyunki agar stack empty hai to fir usme push the element karenge.
         return span;
     }
 };
