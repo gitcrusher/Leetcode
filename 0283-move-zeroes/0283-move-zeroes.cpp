c952bc2aec96fe86abedb9958e582fa246be1994
Aayush Soni <137198498+gitcrusher@users.noreply.github.com>
2026-06-25 09:32:39 +0530

    Time: 0 ms (100%), Space: 23.9 MB (55.27%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int l = 0 ;
+        int r = 0 ; 
+        while(r<nums.size()){
+            if(nums[r]==0 && nums[l]==0)r++;
+            else swap(nums[l++],nums[r++]);
+        }
+    }
+};
